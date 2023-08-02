1ce69607830a8b1c5eacf6cab65be961db827c55
jamshid <jamshidqochqorov84@gmail.com>
2023-08-02 10:45:53 +0500

    project created

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Schema\Builder;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Builder::defaultStringLength(191);
+        // Laravel Pagination use Bootstrap
+        Paginator::useBootstrap();
+
+        // Connect Helpers
+        foreach (glob(__DIR__.'/../Helpers/*.php') as $filename) {
+            require_once $filename;
+        }
+    }
+}
